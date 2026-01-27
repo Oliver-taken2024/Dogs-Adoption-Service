d28130cf65e1103055f45ebafa870f94b2e5d56a
Oliver-taken2024 <oliver.w.pettersson@outlook.com>
2026-01-27 13:46:16 +0100

    Gjort så att när man clickar på adopt dog så kommer man till en sida som congratuler en och visar med information om hunden

--- Dogs-Adoption-Service/Controllers/DogController.cs
+++ Dogs-Adoption-Service/Controllers/DogController.cs
@@ -8,17 +8,24 @@ namespace Dogs_Adoption_Service.Controllers
         //skapa en falsk databas 
         private static List<DogModel> dogs = new List<DogModel>
         {
-            new DogModel { Id = 1, Name ="Jake", Cutness= 7, FavFood="Turkey",FavToy="Tennisball",Temperment=4,IsAdopted=false},
-            new DogModel { Id = 2, Name ="Mira", Cutness= 10, FavFood="Salami",FavToy="Squeky Toy",Temperment=5,IsAdopted=true},
-            new DogModel { Id = 3, Name ="Daisy", Cutness= 6, FavFood="Bones",FavToy="FlipFlops",Temperment=7,IsAdopted=false},
-            new DogModel { Id = 4, Name ="Dennis", Cutness= 8, FavFood="Pasta",FavToy="Fetch Stick",Temperment=3,IsAdopted=false},
-            new DogModel { Id = 5, Name ="Gustave", Cutness= 9,  FavFood="Liverpaste",FavToy="Plush Squirrel",Temperment=8,IsAdopted=true}
+            new DogModel { Id = 1, Name ="Jake", Cutness= 7,FavFood="Turkey",FavToy="Tennisball",Temperment=4,IsAdopted=false},
+            new DogModel { Id = 2, Name ="Mira", Cutness= 10,FavFood="Salami",FavToy="Squeky Toy",Temperment=5,IsAdopted=false},
+            new DogModel { Id = 3, Name ="Daisy", Cutness= 6,FavFood="Bones",FavToy="FlipFlops",Temperment=7,IsAdopted=false},
+            new DogModel { Id = 4, Name ="Dennis", Cutness= 8,FavFood="Pasta",FavToy="Fetch Stick",Temperment=3,IsAdopted=false},
+            new DogModel { Id = 5, Name ="Gustave", Cutness= 9,FavFood="Liverpaste",FavToy="Plush Squirrel",Temperment=8,IsAdopted=false}
         };
 
         public IActionResult Index()
         {
             //skikar listan av dogs till view
             return View(dogs);
+        }
+
+
+        public IActionResult Adoption(int id)
+        {
+            var dog = dogs.FirstOrDefault(m => m.Id == id);
+            return View(dog);
         }
     }
 }

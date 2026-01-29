408d4e1153342f436a2ae0c02eb23f08f579b478
Oliver-taken2024 <oliver.w.pettersson@outlook.com>
2026-01-29 13:11:05 +0100

    nästan klar

--- Dogs-Adoption-Service/Models/DogModel.cs
+++ Dogs-Adoption-Service/Models/DogModel.cs
@@ -6,7 +6,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public int Cutness { get; set; }
-        
+        public string Image { get; set; }
         public string FavFood { get; set; }
         public string FavToy { get; set; }
         public int Temperment { get; set; }
